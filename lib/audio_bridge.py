adec7ccfca480284d46932c707cc203f1369d37d
chicodog530 <chicodog530@yahoo.com>
2026-08-17 16:27:58 -0500

    fix: bind udp socket to RptPort 62032 so DMRGateway accepts packets

--- lib/audio_bridge.py
+++ lib/audio_bridge.py
@@ -92,6 +92,8 @@ class AudioBridge:
     async def brandmeister_client(self, host, port):
         import socket
         sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        sock.bind(("127.0.0.1", 62032))
         sock.setblocking(False)
         
         # Login to local DMRGateway (simulating MMDVMHost)
